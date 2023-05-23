62f99c495e3d48e1964dffad308e0f4e7c77390f
JINKILA <jinkilajeon@gmail.com>
2023-05-23 11:40:25 +0530

    smaller devie desing

---
+++ public/main.js
@@ -0,0 +1,17 @@
+const BTN1 =document.querySelector('.BTN1');
+const BTN2= document.querySelector('.BTN2');
+const BTN3= document.querySelector('.BTN3');
+const BTN4= document.querySelector('.BTN4');
+const BTN5= document.querySelector('.BTN5');
+const BTN6= document.querySelector('.BTN6');
+const BTN7= document.querySelector('.BTN7');
+const BTN8= document.querySelector('.BTN8');
+const BTN9= document.querySelector('.BTN9');
+const BTN10= document.querySelector('.BTN10');
+const BTN11= document.querySelector('.BTN11');
+const BTN12= document.querySelector('.BTN12');
+
+
+
+
+
